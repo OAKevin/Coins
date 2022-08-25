a0589217f51b394f7d590e6af681ac1e0f1b5874
Kevin Miranda <workhorse@Kevins-Mac-mini.attlocal.net>
2022-08-25 11:45:24 -0400

    added the cards to display the coins and changed a few styling issues with the icon size

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -11,8 +11,26 @@ const Cryptocurrencies = () => {
 
   console.log(cryptos);
   return (
-    <div>Cryptocurrencies</div>
-  )
-}
+    <>
+      <Row gutters={[32, 32]} className="crypto-card-container">
+        {cryptos.map((currency) => (
+          <Col xs={24} sm={12} lg={6} classname="crypto-card" key={currency.uuid}>
+            <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
+                <Card 
+                  title={`${currency.rank}. ${currency.name}`}
+                  extra={<img style={{height:30}}className="crypto-image" src={currency.iconUrl}/>}
+                  hoverable
+                >
+                  <p>Price: {millify(currency.price)}</p>
+                  <p>Market Cap: {millify(currency.marketCap)}</p>
+                  <p>Daily Change: {millify(currency.change)}%</p>
+                </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
 export default Cryptocurrencies

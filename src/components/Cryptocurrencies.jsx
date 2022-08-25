075c375b1b07de14c57907ed22a7a05281f09e81
oakevin <kevinmirandat@gmail.com>
2022-08-25 13:26:00 -0400

    update before starting to implement the news Api

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,19 +1,36 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import millify from 'millify';
 import {Link} from 'react-router-dom';
 import {Card, Row, Col, Input} from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
-const Cryptocurrencies = () => {
-  const { data:cryptosList, isFetching} = useGetCryptosQuery();
-  const [cryptos, setCryptos]= useState(cryptosList?.data?.coins);
+const Cryptocurrencies = ({ simplified}) => {
+  const count = simplified ? 10: 100;
+  const { data:cryptosList, isFetching} = useGetCryptosQuery(count);
+  const [cryptos, setCryptos]= useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  
+  {/* Filtering data by search bar  */}
+  useEffect(()=> {
+    const filteredData = cryptosList?.data?.coins.filter((coin)=>coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]);
 
-  console.log(cryptos);
+  if(isFetching) return 'Loading ...';
+  
   return (
     <>
-      <Row gutters={[32, 32]} className="crypto-card-container">
-        {cryptos.map((currency) => (
+      {/* !simplified is to only shows the search bar in the cryptos page  */}
+      {!simplified && (
+        <div className="search-crypto">
+          <input placeholder="Search Cryptocurrency" onChange={(e)=> setSearchTerm(e.target.value)}/>
+        </div>
+      )}
+      
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos ?.map((currency) => (
           <Col xs={24} sm={12} lg={6} classname="crypto-card" key={currency.uuid}>
             <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
                {/* Changed the styling to restrict the size of the card  */}

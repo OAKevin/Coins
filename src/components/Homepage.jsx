9e8e3448c5dc2b46965a0d61a8010a7d0a07a441
Kevin Miranda <workhorse@Kevins-Mac-mini.attlocal.net>
2022-08-25 10:15:37 -0400

    established connection with rapidApi and integrated/tested the fetching of 50 coins"

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,12 +3,15 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic} from 'antd';
 import { Link } from 'react-router-dom';
 
+import { useGetCryptosQuery } from '../services/cryptoApi';
 const { Title } = Typography;
 
 const Homepage = () => {
+  const {data , isFetching} = useGetCryptosQuery();
+  console.log(data);
   return (
     <>
-      <Title level={2} className="heading">Gloval Crypto Stats</Title>
+      <Title level={2} className="heading">Global Crypto Stats</Title>
       <Row>
         <Col span={12}> <Statistic title="Total Cryptocurrencies" value="5"/></Col>
         <Col span={12}> <Statistic title="Total Exchanges" value="5"/></Col>

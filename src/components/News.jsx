5c7e96872e360ccfd6b08bc8b6dc33f40b36e014
oakevin <kevinmirandat@gmail.com>
2022-08-29 16:35:50 -0400

    Adding Loader instead of the hard coded Loading

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
 import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
-
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import Loader from './Loader';
+
 const { Text, Title} = Typography;
 const {Option } = Select;
 
@@ -13,9 +14,7 @@ const News = ({ simplified }) => {
   const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
   const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6: 12});
   const { data} = useGetCryptosQuery(100);
-  
-  if(!cryptoNews ?. value) return 'Loading ...';
-
+  if(!cryptoNews?.value) return <Loader/>;
   return (
     <Row gutter={[ 24,24]}>
       {!simplified &&(

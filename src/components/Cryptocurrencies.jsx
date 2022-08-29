f45da09bc62ea7d739178403203f73a6886dd6ba
oakevin <kevinmirandat@gmail.com>
2022-08-29 12:01:15 -0400

    implemented the Chart feature and fixed the time display to show accurate timing

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -11,9 +11,9 @@ const Cryptocurrencies = ({ simplified}) => {
   const [cryptos, setCryptos]= useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   
-  {/* Filtering data by search bar  */}
   useEffect(()=> {
-    const filteredData = cryptosList?.data?.coins.filter((coin)=>coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    setCryptos(cryptosList?.data?.coins);
+    const filteredData = cryptosList?.data?.coins.filter((item)=>item.name.toLowerCase().includes(searchTerm));
   
     setCryptos(filteredData);
   }, [cryptosList, searchTerm]);
@@ -25,23 +25,23 @@ const Cryptocurrencies = ({ simplified}) => {
       {/* !simplified is to only shows the search bar in the cryptos page  */}
       {!simplified && (
         <div className="search-crypto">
-          <input placeholder="Search Cryptocurrency" onChange={(e)=> setSearchTerm(e.target.value)}/>
+          <Input placeholder="Search Cryptocurrency" onChange={(e)=> setSearchTerm(e.target.value.toLowerCase())}/>
         </div>
       )}
       
       <Row gutter={[32, 32]} className="crypto-card-container">
-        {cryptos ?.map((currency) => (
-          <Col xs={24} sm={12} lg={6} classname="crypto-card" key={currency.uuid}>
+        {cryptos?.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
             <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
                {/* Changed the styling to restrict the size of the card  */}
                 <Card 
                   title={`${currency.rank}. ${currency.name}`}
-                  extra={<img style={{height:30}}className="crypto-image" src={currency.iconUrl}/>}
+                  extra={<img style={{height:30}}className = "crypto-image" src={currency.iconUrl} alt="crypto"/>}
                   hoverable
                 >
                   <p>Price: {millify(currency.price)}</p>
                   <p>Market Cap: {millify(currency.marketCap)}</p>
-                  <p>Daily Change: {millify(currency.change)}%</p>
+                  <p>Daily Change: {currency.change}%</p>
                 </Card>
             </Link>
           </Col>
@@ -51,4 +51,4 @@ const Cryptocurrencies = ({ simplified}) => {
   );
 };
 
-export default Cryptocurrencies
+export default Cryptocurrencies;
--- src/components/Cryptodetails.jsx
+++ src/components/Cryptodetails.jsx
@@ -5,16 +5,21 @@ import millify from 'millify';
 import{ Col, Row, Typography, Select} from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
-import {useGetCryptoDetailsQuery} from '../services/cryptoApi';
+import {useGetCryptoDetailsQuery, useGetCryptoHistoryQuery} from '../services/cryptoApi';
+import LineChart from './LineChart';
+
 const { Title, Text } = Typography;
 const { Option} = Select;
 
 
 const CryptoDetails = () => {
   const { coinId} = useParams();
-  const [ timeperiod, setTimePeriod] = useState('7d');
+  const [ timePeriod, setTimePeriod] = useState('7d');
   const {data, isFetching} = useGetCryptoDetailsQuery(coinId);
+  const {data: coinHistory} = useGetCryptoHistoryQuery({coinId, timePeriod});
   const cryptoDetails = data?.data?.coin;
+
+  if(isFetching) return 'Loading ...';
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
@@ -23,13 +28,13 @@ const CryptoDetails = () => {
     { title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined /> },
     { title: '24h Volume', value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`, icon: <ThunderboltOutlined /> },
     { title: 'Market Cap', value: `$ ${cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)}`, icon: <DollarCircleOutlined /> },
-    { title: 'All-time-high(daily avg.)', value: `$ ${cryptoDetails?.allTimeHigh?.price && millify(cryptoDetails?.allTimeHigh?.price)}`, icon: <TrophyOutlined /> },
+    { title: 'ATH(daily avg.)', value: `$ ${cryptoDetails?.allTimeHigh?.price && millify(cryptoDetails?.allTimeHigh?.price)}`, icon: <TrophyOutlined /> },
   ];
 
   const genericStats = [
     { title: 'Number Of Markets', value: cryptoDetails?.numberOfMarkets, icon: <FundOutlined /> },
     { title: 'Number Of Exchanges', value: cryptoDetails?.numberOfExchanges, icon: <MoneyCollectOutlined /> },
-    { title: 'Aprroved Supply', value: cryptoDetails?.supply?.confirmed ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+    { title: 'Aproved Supply', value: cryptoDetails?.supply?.confirmed ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
     { title: 'Total Supply', value: `$ ${cryptoDetails?.supply?.total && millify(cryptoDetails?.supply?.total)}`, icon: <ExclamationCircleOutlined /> },
     { title: 'Circulating Supply', value: `$ ${cryptoDetails?.supply?.circulating && millify(cryptoDetails?.supply?.circulating)}`, icon: <ExclamationCircleOutlined /> },
   ];
@@ -53,7 +58,7 @@ const CryptoDetails = () => {
       >
         {time.map((date)=> <Option key={date}>{date}</Option>)}
       </Select>
-      {/* line chart.... */}
+     <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails.name}/>
       <Col className="stats-container">
         <Col className="coin-value-statics">
           <Col className="coin-value=statics-heading">
@@ -114,7 +119,7 @@ const CryptoDetails = () => {
               <Title level={5} className="link-name">
                 {link.type}
               </Title>
-              <a href={link.url} target="_blank" rel="noreffer">
+              <a href={link.url} target="_blank" rel="noreferrer">
                 {link.name}
               </a>
             </Row>
@@ -122,7 +127,7 @@ const CryptoDetails = () => {
         </Col>
       </Col>
     </Col>
-  )
-}
+  );
+};
 
 export default CryptoDetails
